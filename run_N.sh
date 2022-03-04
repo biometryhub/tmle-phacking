12b90efbd95faf25d6c4c03a56e0e415418761df
Russell Andrew Edson <russell.andrew.edson@pm.me>
2022-03-04 18:47:14 +1030

    Separation between host output and docker output dirs

--- run_N.sh
+++ run_N.sh
@@ -14,17 +14,17 @@ HOST_WD="${HOME}/tmle-phacking"
 DOCKER_WD="${HOME}/run"
 OUTPUT_DIR="output_${N}"
 HOST_OUT="${HOST_WD}/${OUTPUT_DIR}"
-DOCKER_OUT="${DOCKER_OUT}/${OUTPUT_DIR}"
+DOCKER_OUT="${DOCKER_WD}/${OUTPUT_DIR}"
 HOST_LOG="${HOST_OUT}/shell_log.txt"
 DOCKER_LOG="${DOCKER_OUT}/shell_log.txt"
 R_SCRIPT="${DOCKER_WD}/simulate_TMLE.R"
 
+sudo chmod -R ugo+rw "${HOST_WD}"
+mkdir "${HOST_OUT}"
 echo "$(uname -a)" >> "${HOST_LOG}"
 echo "$(lscpu)" >> "${HOST_LOG}"
 
-sudo chmod -R ugo+rw "${HOST_WD}"
 sudo docker run --name "tmle-phacking-run${N}" -it -d \
   -v "${HOST_WD}:${DOCKER_WD}" tmle-phacking
-sudo docker exec "tmle-phacking-run${N}" sh -c "mkdir ${DOCKER_OUT}"
 sudo nohup docker exec "tmle-phacking-run${N}" sh -c \
   "R -e \"source('${R_SCRIPT}'); run_compute(${N}, '${DOCKER_OUT}')\" 2&>1 >> ${DOCKER_LOG}"
