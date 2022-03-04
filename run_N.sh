7c30f2551f940b9974a5ec81ec1b5c31300eaefb
Russell Andrew Edson <russell.andrew.edson@pm.me>
2022-03-04 18:42:06 +1030

    Fixed up host-side logging

--- run_N.sh
+++ run_N.sh
@@ -10,17 +10,21 @@
 # Code author: Russell A. Edson, Biometry Hub
 # Date last modified: 04/03/2022
 N="$1"
-GIT_DIR="${HOME}/tmle-phacking"
-OUTPUT_DIR="${HOME}/run/output_${N}"
-LOG="${OUTPUT_DIR}/shell_log.txt"
-R_SCRIPT="${HOME}/run/simulate_TMLE.R"
+HOST_WD="${HOME}/tmle-phacking"
+DOCKER_WD="${HOME}/run"
+OUTPUT_DIR="output_${N}"
+HOST_OUT="${HOST_WD}/${OUTPUT_DIR}"
+DOCKER_OUT="${DOCKER_OUT}/${OUTPUT_DIR}"
+HOST_LOG="${HOST_OUT}/shell_log.txt"
+DOCKER_LOG="${DOCKER_OUT}/shell_log.txt"
+R_SCRIPT="${DOCKER_WD}/simulate_TMLE.R"
 
-echo "$(uname -a)" >> "${LOG}"
-echo "$(lscpu)" >> "${LOG}"
+echo "$(uname -a)" >> "${HOST_LOG}"
+echo "$(lscpu)" >> "${HOST_LOG}"
 
-sudo chmod -R ugo+rw "${GIT_DIR}"
+sudo chmod -R ugo+rw "${HOST_WD}"
 sudo docker run --name "tmle-phacking-run${N}" -it -d \
-  -v "${GIT_DIR}:${HOME}/run" tmle-phacking
-sudo docker exec "tmle-phacking-run${N}" sh -c "mkdir ${OUTPUT_DIR}"
+  -v "${HOST_WD}:${DOCKER_WD}" tmle-phacking
+sudo docker exec "tmle-phacking-run${N}" sh -c "mkdir ${DOCKER_OUT}"
 sudo nohup docker exec "tmle-phacking-run${N}" sh -c \
-  "R -e \"source('${R_SCRIPT}'); run_compute(${N}, '${OUTPUT_DIR}')\" 2&>1 >> ${LOG}"
+  "R -e \"source('${R_SCRIPT}'); run_compute(${N}, '${DOCKER_OUT}')\" 2&>1 >> ${DOCKER_LOG}"
