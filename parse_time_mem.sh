72490e78b4b46639bd7576a2ed02cc6b933f486a
RussellAndrewEdson <russell.andrew.edson@gmail.com>
2022-03-31 17:19:10 +1030

    Quick script to compare execution times/memory for the superlearners

---
+++ parse_time_mem.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Quick awk script to parse the time and memory usage for each
+# of the SuperLearner combinations given the R_log.txt text file.
+#
+# Run with ./parse_time_mem.sh R_log.txt
+#
+# Code author: Russell A. Edson, Biometry Hub
+# Date last modified: 31/03/2022
+FILE="$1"
+REGEX='/(?<=comb=)(\d+).*(?<=time:)([\d+|.]+).*(?<=usage: )(\d+)/'
+echo "n,time,memory"
+cat "$FILE" |
+    awk 'BEGIN { RS="\n\n"; FS="\n" } /SLcomb=/ { print $1"|"$3"|"$4 }' |
+    perl -ne "${REGEX} and print \"\$1,\$2,\$3\n\""
