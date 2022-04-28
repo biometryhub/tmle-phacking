01908242f4e0f2da19ca043b78185f83220dc0bb
RussellAndrewEdson <russell.andrew.edson@gmail.com>
2022-04-28 15:27:33 +0930

    Modified regex to handle uneven spacing in R output sometimes

--- parse_time_mem.sh
+++ parse_time_mem.sh
@@ -7,7 +7,7 @@
 # Code author: Russell A. Edson, Biometry Hub
 # Date last modified: 28/04/2022
 FILE="$1"
-REGEX='/(?<=comb=)(\d+).*(?<=time:)([\d+|.]+).*(?<=usage: )([^\r\n]+)/'
+REGEX='/(?<=comb=)(\d+).*(?<=time:)[ ]*([\d+|.]+).*(?<=usage: )([^\r\n]+)/'
 echo "n,time,memory"
 cat "$FILE" |
     awk 'BEGIN { RS="\n\n"; FS="\n" } /SLcomb=/ { print $1"|"$3"|"$4 }' |
